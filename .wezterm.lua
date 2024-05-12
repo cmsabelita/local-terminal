f11e3a7ffb3af25be60b0d49cf48d52baa409897
cmsabelita <cmsabelita@gmail.com>
2024-05-12 12:53:29 +0800

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
+-- Pull in the wezterm API
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -7,44 +7,50 @@ local act = wezterm.action
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "Dracula (Official)"
+-- config.color_scheme = "Dracula (Official)"
+config.color_scheme = "Abernathy"
+-- config.color_scheme = "Catppuccin Latte"
+
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
-config.window_decorations = "RESIZE"
 config.font_dirs = { "~/Library/Fonts" }
 config.freetype_load_target = "HorizontalLcd"
-config.font_size = 12
+config.font_size = 14
 config.line_height = 1
-config.font = wezterm.font("SauceCodePro Nerd Font", { weight = "DemiBold", stretch = "Normal" }) -- (AKA: SauceCodePro NF ExtraLight) /Users/mark/Library/Fonts/SauceCodeProNerdFont-ExtraLightItalic.ttf, CoreText
-config.window_background_opacity = 0.88
-config.initial_rows = 90
-config.initial_cols = 200
+config.font = wezterm.font("SauceCodePro Nerd Font", { stretch = "Normal" })
+config.window_background_opacity = 0.91
+config.initial_rows = 65
+config.initial_cols = 150
 config.cell_width = 1
 config.audible_bell = "Disabled"
 config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
 
--- config.macos_window_background_blur =
--- config.window_background_opacity = 0.1
-config.macos_window_background_blur = 7
+config.macos_window_background_blur = 28
 config.keys = {
-        {
-                key = "e",
-                mods = "CMD",
-                action = act.PaneSelect({
-                        alphabet = "asdf",
-                }),
-        },
-        -- This will create a new split and run your default program inside it
-        {
-                key = "d",
-                mods = "CMD",
-                action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-        },
-        {
-                key = "p",
-                mods = "CMD",
-                action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-        },
+	{
+		key = "e",
+		mods = "CMD",
+		action = act.PaneSelect({
+			alphabet = "asdf",
+		}),
+	},
+	{
+		key = "d",
+		mods = "CMD",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "p",
+		mods = "CMD",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
 }
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function()
+	local tab, pane, window = mux.spawn_window({})
+	window:gui_window():maximize()
+end)
 
 return config
